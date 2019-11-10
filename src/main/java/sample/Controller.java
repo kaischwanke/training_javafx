c44f42be9293a5cdec94d5dd919fd0edeb265772
Kai Schwanke <kai.schwanke@gmail.com>
2019-11-10 21:03:58 +1100

    exercise 1

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import com.google.common.primitives.Ints;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class Controller {
 
@@ -16,11 +19,25 @@ public class Controller {
     private TextField output;
 
     @FXML
+    private TextFlow intro;
+
+    @FXML
+    private TextFlow info;
+
+    @FXML
     private void initialize()
     {
+        intro.getChildren().add(new Text("Add 2 text input fields \n" +
+                "(with IDs 'inputOne' and 'inputTwo'), \n" +
+                "a button and another text field (ID 'output',read-only)\n" +
+                "The button should call the action with ID 'add"));
         if (inputOne == null || inputTwo == null || output == null) {
-            throw new RuntimeException("Fields not configured correctly.");
+            Text error = new Text("Fields not configured correctly");
+            info.getChildren().add(error);
+            error.setFill(Color.RED);
+            //throw new RuntimeException("Fields not configured correctly.");
         }
+
         System.out.println("init complete");
     }
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResources("sample.fxml").nextElement());
         primaryStage.setTitle("Calculator");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 450, 375));
         primaryStage.show();
     }
 
